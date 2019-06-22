12493153b49fb118e04d889d5ca95473ee21d926
Gabriela <gabi_guti_1@hotmail.com>
2019-06-22 18:56:52 +0200

    Setting "funcionarios" table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,6 @@ import {Funcionario} from './funcionario';
 import {AppService} from './app.service';
 import {MatTableDataSource} from '@angular/material';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -32,33 +12,25 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class AppComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-
-
+  displayedColumns: string[] = ['id', 'nome', 'idade', 'cargo'];
+  dataSource: MatTableDataSource;
   title = 'StoneChallenge';
   funcionarios: Funcionario[];
   funcionarioSelecionado: Funcionario;
 
-  tableColumns: string[] = ['id', 'nome', 'idade', 'cargo'];
-
   constructor (private service: AppService, private http: HttpClient) {}
 
   ngOnInit() {
+    this.dataSource =  new MatTableDataSource(this.funcionarios);
     this.AtualizarListaFuncionarios();
   }
 
   // Pegar lista de funcionarios da base de dados (DB)
   AtualizarListaFuncionarios() {
-    console.log("GETTING THEM");
     this.service.getFuncionarios().subscribe(data => {
         this.funcionarios = data;
-        console.log("THEM", data, this.funcionarios);
+        this.dataSource =  new MatTableDataSource(this.funcionarios);
       });
-  }
-
-  test() {
-    console.log("FUNCIOANRIOS", this.funcionarios);
   }
 
   // Adicionar funcionario a base de dados e atualizar lista de funcionarios
